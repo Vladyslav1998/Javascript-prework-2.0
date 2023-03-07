4a9d8e7beb3e2c2d1562a47c2ad7ddbf3e1c920d
Vladyslav Mazurenko <vladmazurenko13798@gmail.com>
2023-03-07 01:30:19 +0100

    Share code from script.js to functions.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-function printMessage(msg) {
-   let div = document.createElement('div');
-   div.innerHTML = msg;
-   document.getElementById('messages').appendChild(div);
-}
 
-function clearMessages() {
-   document.getElementById('messages').innerHTML = '';
-}
