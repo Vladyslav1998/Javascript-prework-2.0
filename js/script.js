ea6ea623f6fed929862db25f3101c4214950629b
Vladyslav Mazurenko <vladmazurenko13798@gmail.com>
2023-03-15 21:11:44 +0100

    Add function computerMove in JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
 let playerMove = 'papier';
 
 pringMessage('Zagrałem ' + playerMove + ', Twoja kolej!')
