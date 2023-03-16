356c5d61ff6b6210ab0aeadfce8cc8503ae8e791
Vladyslav Mazurenko <vladmazurenko13798@gmail.com>
2023-03-16 14:46:50 +0100

    Add loop for 'computerMove ion JS'

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowany numer to: ' + randomNumber);
+console.log('Wylosowana liczba to: ' + randomNumber);
 
+let computerMove = 'nieznany ruch'
+
+if(randomNumber == 1){
+   computerMove = 'kamień';
+}
+
+else if(randomNumber == 2){
+   computerMove = "papier";
+}
+
+else if(randomNumber == 3){
+   computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
