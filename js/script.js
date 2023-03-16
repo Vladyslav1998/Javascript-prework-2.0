7229fbcbd25343d29407e68137aa9a8d8950b11d
Vladyslav Mazurenko <vladmazurenko13798@gmail.com>
2023-03-16 14:24:09 +0100

    Change code of game in JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-let computerMove = `kamień`;
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-let playerMove = 'papier';
-
-printMessage('Zagrałem ' + playerMove + ', Twoja kolej!')
-
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Całość to ' + randomNumber);
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+   computerMove = 'kamień';
+}
+else if (randomNumber == 2) {
+   computerMove = 'papier';
+}
+
+else if (randomNumber == 3) {
+   computerMove = 'nożyce';
+}
+printMessage('Mój ruch to: ' + computerMove);
