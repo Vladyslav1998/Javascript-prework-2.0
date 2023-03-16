ae03c2b9315e4db4cdd3e3704d3e93bd4a8d6261
Vladyslav Mazurenko <vladmazurenko13798@gmail.com>
2023-03-16 14:36:41 +0100

    Add function 'randomNumber' in JS

--- js/script.js
+++ js/script.js
@@ -1,17 +1,4 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+console.log('Wylosowany numer to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
-
-if (randomNumber == 1) {
-   computerMove = 'kamień';
-}
-else if (randomNumber == 2) {
-   computerMove = 'papier';
-}
-
-else if (randomNumber == 3) {
-   computerMove = 'nożyce';
-}
-printMessage('Mój ruch to: ' + computerMove);
